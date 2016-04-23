0c05f0f99d3b26479f10e06f4b1aec868bc0c6ce
ShunSakurai <justmyway.yui@gmail.com>
2016-04-23 23:54:38 +0900

    Reduce the size of the dist folder and the .app file

--- setup_win.py
+++ setup_win.py
@@ -1,17 +1,24 @@
 '''
 cd Dropbox/Codes/password
 py -3.4 setup_win.py py2exe
+
+Libraries used:
+import tkinter
+from random import sample
+from re import search
 '''
 from distutils.core import setup
 import py2exe
 
-SCRIPT = 'password_generator.py'
-VERSION = "1.1.3"
-DATA_FILES = []
-NAME = "Password Generator"
-OPTIONS = {'bundle_files': 2}
-
 setup(
-      console=[{'script': SCRIPT, 'version': VERSION, 'dest_base': NAME}],
-      options={'py2exe': OPTIONS},
+      console=[{'author': 'Shun Sakurai',
+                       'dest_base': 'Password Generator',
+                       'script': 'password_generator.py',
+                       'version': '1.1.4'
+                       }],
+      options={'py2exe': {
+                     'bundle_files': 2,
+                     'compressed': True,
+                     'excludes': ['_hashlib', '_frozen_importlib', 'argparse', '_lzma', '_bz2', '_ssl', 'calendar', 'datetime', 'difflib', 'doctest', 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline', 'zipfile'],
+                     }}
       )
