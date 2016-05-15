d3315416417b645f36b0890e9c9cd923c424a240
ShunSakurai <justmyway.yui@gmail.com>
2016-05-15 09:33:54 +0900

    Correct indentation

--- setup_mac.py
+++ setup_mac.py
@@ -14,9 +14,9 @@ setup(
     app=['password_generator.py'],
     name='Password Generator',
     options={'py2app': {
-                   'argv_emulation': False,
-                   'excludes': ['_bz2', '_frozen_importlib', '_hashlib', '_lzma', '_ssl', 'argparse', 'calendar', 'datetime', 'difflib', 'doctest', 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline', 'zipfile'],
-                   }},
+        'argv_emulation': False,
+        'excludes': ['_bz2', '_frozen_importlib', '_hashlib', '_lzma', '_ssl', 'argparse', 'calendar', 'datetime', 'difflib', 'doctest', 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline', 'zipfile'],
+    }},
     setup_requires=['py2app'],
     version='1.2.0',
 )
--- setup_win.py
+++ setup_win.py
@@ -11,14 +11,15 @@ from distutils.core import setup
 import py2exe
 
 setup(
-      console=[{'author': 'Shun Sakurai',
-                       'dest_base': 'Password Generator',
-                       'script': 'password_generator.py',
-                       'version': '1.2.0'
-                       }],
-      options={'py2exe': {
-                     'bundle_files': 2,
-                     'compressed': True,
-                     'excludes':['_bz2', '_frozen_importlib', '_hashlib', '_lzma', '_ssl', 'argparse', 'calendar', 'datetime', 'difflib', 'doctest', 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline', 'zipfile'],
-                     }}
-      )
+    console=[{
+        'author': 'Shun Sakurai',
+        'dest_base': 'Password Generator',
+        'script': 'password_generator.py',
+        'version': '1.2.0'
+    }],
+    options={'py2exe': {
+        'bundle_files': 2,
+        'compressed': True,
+        'excludes':['_bz2', '_frozen_importlib', '_hashlib', '_lzma', '_ssl', 'argparse', 'calendar', 'datetime', 'difflib', 'doctest', 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline', 'zipfile'],
+    }}
+)
